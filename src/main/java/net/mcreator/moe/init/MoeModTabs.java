3f8028ea39514afe55b25eaebbec9c6b303e6ab9
Bufka2011 <bufka.bulanov@gmail.com>
2023-07-07 23:22:16 +0300

    Gamma 0.0.5 localization update!

--- src/main/java/net/mcreator/moe/init/MoeModTabs.java
+++ src/main/java/net/mcreator/moe/init/MoeModTabs.java
@@ -9,29 +9,18 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.event.CreativeModeTabEvent;
 
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.CreativeModeTabs;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.network.chat.Component;
 
 @Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class MoeModTabs {
 	@SubscribeEvent
-	public static void buildTabContentsVanilla(CreativeModeTabEvent.BuildContents tabData) {
-
-		if (tabData.getTab() == CreativeModeTabs.BUILDING_BLOCKS) {
-			tabData.accept(MoeModBlocks.BUG.get().asItem());
-		}
-
-		if (tabData.getTab() == CreativeModeTabs.NATURAL_BLOCKS) {
-			tabData.accept(MoeModBlocks.BUTTER_MUSHROOM.get().asItem());
-		}
-	}
-
-	@SubscribeEvent
 	public static void buildTabContentsModded(CreativeModeTabEvent.Register event) {
 		event.registerCreativeModeTab(new ResourceLocation("moe", "mod_of_everything"),
 				builder -> builder.title(Component.translatable("item_group.moe.mod_of_everything")).icon(() -> new ItemStack(MoeModBlocks.BUG.get())).displayItems((parameters, tabData) -> {
+					tabData.accept(MoeModBlocks.BUG.get().asItem());
 					tabData.accept(MoeModItems.TAP.get());
+					tabData.accept(MoeModBlocks.BUTTER_MUSHROOM.get().asItem());
 					tabData.accept(MoeModItems.BUTTER.get());
 				})
 
