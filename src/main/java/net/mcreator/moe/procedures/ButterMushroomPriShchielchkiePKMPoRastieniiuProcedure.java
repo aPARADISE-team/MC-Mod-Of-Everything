380a666458d36c2c7442f350a9c681dcc563f482
Bufka2011 <bufka.bulanov@gmail.com>
2023-07-07 23:18:01 +0300

    Gamma 0.0.4 update!

--- src/main/java/net/mcreator/moe/procedures/ButterMushroomPriShchielchkiePKMPoRastieniiuProcedure.java
+++ src/main/java/net/mcreator/moe/procedures/ButterMushroomPriShchielchkiePKMPoRastieniiuProcedure.java
@@ -1,29 +1,10 @@
 package net.mcreator.moe.procedures;
 
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.eventbus.api.Event;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.network.chat.Component;
 
-import javax.annotation.Nullable;
-
-@Mod.EventBusSubscriber
 public class ButterMushroomPriShchielchkiePKMPoRastieniiuProcedure {
-	@SubscribeEvent
-	public static void onRightClickBlock(PlayerInteractEvent.RightClickBlock event) {
-		if (event.getHand() != event.getEntity().getUsedItemHand())
-			return;
-		execute(event, event.getLevel());
-	}
-
 	public static void execute(LevelAccessor world) {
-		execute(null, world);
-	}
-
-	private static void execute(@Nullable Event event, LevelAccessor world) {
 		if (!world.isClientSide() && world.getServer() != null)
 			world.getServer().getPlayerList().broadcastSystemMessage(Component.literal("\u041D\u0430 \u043E\u0449\u0443\u043F\u044C \u043A\u0430\u043A \u043C\u0430\u0441\u043B\u043E."), false);
 	}
